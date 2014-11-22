aff2dd6214a69217fd9869d12290deae6afc15ed
Manish Gill <mgill25@outlook.com>
2014-11-23 05:17:30 +0530

    Refactored each view to have title via inserting :title variable in layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Rails Hello World"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Rails Hello World"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Rails Hello World"
   end
 
 end
