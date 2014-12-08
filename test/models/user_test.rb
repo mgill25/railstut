20767944cc2f4229f7125484165d47730fc4b555
Manish Gill <mgill25@outlook.com>
2014-12-09 04:41:51 +0530

    Tutorial on ActiveRecords - Model creation, testing, schema migration

---
+++ test/models/user_test.rb
@@ -0,0 +1,65 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # TDD is perfect for Database Model validation.
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = nil
+    assert_not @user.valid?
+    @user.name = ""
+    assert_not @user.valid?
+    @user.name = "Example User"
+    assert @user.valid?
+  end
+
+  test "name shouldn't be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 256
+    assert_not @user.valid?
+  end
+
+  test "should take valid emails only" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+
+    valid_addresses.each do |addr|
+      @user.email = addr
+      assert @user.valid?, "#{addr.inspect} should be valid"
+    end
+  end
+
+  test "email should be unique" do
+    dup_user = @user.dup
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "email should be case-insensitive" do
+    dup_user = @user.dup
+    dup_user.email = @user.email.upcase
+    @user.save
+    assert_not dup_user.valid?
+  end
+
+  test "should have minimum password length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "user should be authenticated with password provided" do
+    assert @user.authenticate("foobar")
+    assert_not @user.authenticate("thisorthat")
+  end
+end
