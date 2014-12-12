8ca7fc862c9249ab8ad1bf4fce76f4b5c6ab873c
Manish Gill <mgill25@outlook.com>
2014-12-13 01:59:54 +0530

    Invalid Signup Test. Redirection from signup to profile. Flash message.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,15 @@ Rails.application.routes.draw do
 
   # Resource routes
   resources :users
+  # ^ will automatically create the following...
+  #  HTTP    URL            Action   Named route           Purpose
+  #  GET     /users         index    users_path            page to list all users
+  #  GET     /users/1       show     user_path(user)       page to show user
+  #  GET     /users/new     new      new_user_path         page to make a new user(signup)
+  #  POST    /users         create   users_path            create a new user
+  #  GET     /users/1/edit  edit     edit_user_path(user)  page to edit user with id 1
+  #  PATCH   /users/1       update   user_path(user)       update user
+  #  DELETE  /users/1       destroy  user_path(user)       delete user
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
