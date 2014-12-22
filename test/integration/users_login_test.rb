f65342801c37d488556b0b6cf009081c401d69f4
Manish Gill <mgill25@outlook.com>
2014-12-23 01:42:18 +0530

    Added logout and related test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,10 +18,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?  # no flash the second time around!
   end
 
-  test "login with valid information" do
+  test "login with valid information and then log out" do
+    # first test login
     get login_path
     assert_template "sessions/new"
     post login_path, session: { email: @user.email, password: "password" }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template "users/show"
@@ -29,5 +31,14 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path, count: 0     # login link disappears
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    # now log out
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
